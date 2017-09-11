7e58d86d042440ed18a10165179bdf723a419024
Paul Besret <paul.besret@hotmail.fr>
2017-09-11 12:19:15 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,12 @@
 # Colors
 BLACK    = (   0,   0,   0) 
 WHITE    = ( 255, 255, 255) 
- 
+
 # Screen dimensions
 SCREEN_WIDTH  = 800
 SCREEN_HEIGHT = 600
+
+# Tiles constants
+WALL = "#"
+DOOR = "D"
+FLOOR = "X"
